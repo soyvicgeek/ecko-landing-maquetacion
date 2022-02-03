63e04e0830ec65243dd00a71c84157923a893276
Victor Flores <vflorestrejo@gmail.com>
2022-02-03 03:58:56 -0600

    get Data mediante fetch de la api de Rick and Morty para el Módulo de Personaje

--- js/main.js
+++ js/main.js
@@ -24,4 +24,47 @@ $(document).ready(function () {
       $(this).removeClass("active");
     }
   });
+
+  async function getData() {
+    const data = await fetch("https://rickandmortyapi.com/api/character/2");
+    const json = await data.json();
+    const { name, image, gender, episode } = await json;
+
+    // const tpl = episode
+    //   .map((episodio) => `<li>${episodio}</li>`);
+
+    let html = `
+    <img src="${image}" alt="Personaje" class="information__character-body_img">
+
+      <div class="information__character-body_information">
+
+        <h3 class="information__character-body_information-title">Nombre:</h3>
+        <p class="information__character-body_information-paragraphs">${name}</p>
+        <h3 class="information__character-body_information-title">Género:</h3>
+        <p class="information__character-body_information-paragraphs">${gender}</p>
+        <h3 class="information__character-body_information-title">Primeros 5 episodios:</h3>
+        <ul>
+         <li>
+          <a href="${episode[0]}" target="_blank">${episode[0]}</a>
+         </li>
+         <li>
+          <a href="${episode[1]}" target="_blank">${episode[1]}</a>
+         </li>
+         <li>
+          <a href="${episode[2]}" target="_blank">${episode[2]}</a>
+         </li>
+         <li>
+          <a href="${episode[3]}" target="_blank">${episode[3]}</a>
+         </li>
+         <li>
+          <a href="${episode[4]}" target="_blank">${episode[4]}</a>
+         </li>
+        </ul>
+      </div>
+    `;
+
+    document.querySelector("#character").innerHTML = html;
+  }
+
+  getData();
 });
