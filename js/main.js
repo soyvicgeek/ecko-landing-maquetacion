7e8443163687c00237764daf92b5fb82d57fbb0f
Victor Flores <vflorestrejo@gmail.com>
2022-02-02 21:06:20 -0600

    Modulo 'Los más vendidos añadido'

--- js/main.js
+++ js/main.js
@@ -3,6 +3,17 @@ $(document).ready(function () {
     dots: true,
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
   });
 });
